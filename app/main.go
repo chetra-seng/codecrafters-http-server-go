473a626ec3278e9e6c96fc36071e16e38917131d
chetra-seng <dev@chetraseng.com>
2025-04-08 12:57:56 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,8 +4,39 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
+
+/* Request body sample
+// Request line
+GET                          // HTTP method
+/index.html                  // Request target
+HTTP/1.1                     // HTTP version
+\r\n                         // CRLF that marks the end of the request line
+
+// Headers
+Host: localhost:4221\r\n     // Header that specifies the server's host and port
+User-Agent: curl/7.64.1\r\n  // Header that describes the client's user agent
+Accept: _something_\r\n              // Header that specifies which media types the client can accept
+\r\n                         // CRLF that marks the end of the headers
+
+// Request body (empty)
+*/
+
+/* Response body sample
+// Status line
+HTTP/1.1 200 OK
+\r\n                          // CRLF that marks the end of the status line
+
+// Headers
+Content-Type: text/plain\r\n  // Header that specifies the format of the response body
+Content-Length: 3\r\n         // Header that specifies the size of the response body, in bytes
+\r\n                          // CRLF that marks the end of the headers
+
+// Response body
+abc                           // The string from the request
+*/
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
@@ -28,20 +59,30 @@ func main() {
 		os.Exit(1)
 	}
 
+	// TODO: Refactor the code to extract header, body
 	req := make([]byte, 1024)
 	conn.Read(req)
-	reqString := string(req)
+	header, _ := extractRequest(req)
 
-	reqParts := strings.Split(reqString, "\r\n")
+	reqParts := strings.Split(header, "\r\n")
 	reqLine := reqParts[0]
 
 	reqLineParts := strings.Split(reqLine, " ")
 	path := reqLineParts[1]
 
-	if path == "/" {
+	switch {
+	case path == "/":
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else {
+	case strings.HasPrefix(path, "/echo/"):
+		str := strings.TrimPrefix(path, "/echo/")
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(str)) + "\r\n\r\n" + str))
+	default:
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
+}
 
+func extractRequest(req []byte) (string, string) {
+	reqString := string(req)
+	reqParts := strings.Split(reqString, "\r\n\r\n")
+	return reqParts[0], reqParts[1]
 }
