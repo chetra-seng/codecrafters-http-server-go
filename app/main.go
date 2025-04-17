4ea431285a25b926f3e1ce6707392d2383ae88ea
chetra-seng <dev@chetraseng.com>
2025-04-17 20:28:52 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -47,19 +47,26 @@ var _ = os.Exit
 
 const supportedCompression = "gzip"
 
+// NOTE: Please don't judge me
+// I'll refactor my code later :D
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
 	for {
-		go handleConnection(l)
+
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 
 }
@@ -80,27 +87,32 @@ func extractRequest(req []byte) (string, map[string]string, string) {
 	headerParts := reqParts[1 : partLen-2]
 	for _, header := range headerParts {
 		headerParts := strings.Split(header, ": ")
-		headers[headerParts[0]] = headerParts[1]
+		if len(headerParts) == 2 {
+			headers[headerParts[0]] = headerParts[1]
+		}
 	}
 	body := reqParts[partLen-1]
 	return reqLine, headers, body
 }
 
-func handleConnection(l net.Listener) {
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	defer conn.Close()
-
-	req := make([]byte, 1024)
+func handleConnection(conn net.Conn) {
 
 	for {
+		req := make([]byte, 1024)
+		resHeader := make(map[string]string)
 		// NOTE: This slice will pad '\x00' aka 'null zero' for unused space
 
-		conn.Read(req)
+		n, err := conn.Read(req)
+		if err != nil {
+			if err.Error() == "EOF" {
+				conn.Close()
+				return
+			}
+			fmt.Println("Error reading request: ", err.Error())
+			conn.Close()
+			return
+		}
+		req = req[:n]
 		reqLine, headers, body := extractRequest(req)
 
 		reqLineParts := strings.Split(reqLine, " ")
@@ -108,13 +120,14 @@ func handleConnection(l net.Listener) {
 
 		closeCon := false
 
-		if value, ok := headers["Connection"]; ok && value == "Close" {
+		if value, ok := headers["Connection"]; ok && value == "close" {
 			closeCon = true
 		}
 
 		switch {
 		case path == "/":
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			writeResponse(conn, "HTTP/1.1 200 OK", resHeader, "", closeCon)
+
 		case strings.HasPrefix(path, "/echo"):
 			str := strings.TrimPrefix(path, "/echo/")
 			if val, ok := headers["Accept-Encoding"]; ok {
@@ -125,21 +138,36 @@ func handleConnection(l net.Listener) {
 					_, err := gw.Write([]byte(str))
 					gw.Close()
 					if err != nil {
-						conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+						writeResponse(conn, "HTTP/1.1 500 Internal Server Error", resHeader, "", closeCon)
 						break
 					}
-					conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: " + strconv.Itoa(len(b.String())) + "\r\n\r\n" + b.String()))
+
+					// Construct new resHeader
+					resHeader["Content-Type"] = "text/plain"
+					resHeader["Content-Encoding"] = "gzip"
+					resHeader["Content-Length"] = strconv.Itoa(len(b.String()))
+					writeResponse(conn, "HTTP/1.1 200 OK", resHeader, b.String(), closeCon)
 					break
 				}
 			}
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(str)) + "\r\n\r\n" + str))
+
+			resHeader["Content-Type"] = "text/plain"
+			resHeader["Content-Encoding"] = "gzip"
+			resHeader["Content-Length"] = strconv.Itoa(len(str))
+			delete(resHeader, "Content-Encoding")
+			writeResponse(conn, "HTTP/1.1 200 OK", resHeader, str, closeCon)
+
 		case strings.HasPrefix(path, "/user-agent"):
 			agent := ""
 
 			if val, ok := headers["User-Agent"]; ok {
 				agent = val
 			}
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(agent)) + "\r\n\r\n" + agent))
+
+			resHeader["Content-Type"] = "text/plain"
+			resHeader["Content-Length"] = strconv.Itoa(len(agent))
+			writeResponse(conn, "HTTP/1.1 200 OK", resHeader, agent, closeCon)
+
 		case strings.HasPrefix(path, "/files/"):
 			dir := os.Args[2]
 			fileName := dir + strings.TrimPrefix(path, "/files/")
@@ -148,32 +176,58 @@ func handleConnection(l net.Listener) {
 			if method == "GET" {
 				_, err := os.Stat(fileName)
 				if err != nil {
-					conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+					writeResponse(conn, "HTTP/1.1 404 Not Found", resHeader, "", closeCon)
 					break
 				}
 				content, err := os.ReadFile(fileName)
 				if err != nil {
-					conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+					writeResponse(conn, "HTTP/1.1 500 Internal Server Error", resHeader, "", closeCon)
 					break
 				}
-				conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: application/octet-stream\r\nContent-Length: " + strconv.Itoa(len(content)) + "\r\n\r\n" + string(content)))
+
+				// Construct new resHeader
+				resHeader["Content-Type"] = "application/octet-stream"
+				resHeader["Content-Length"] = strconv.Itoa(len(content))
+				writeResponse(conn, "HTTP/1.1 200 OK", resHeader, string(content), closeCon)
 			} else if method == "POST" {
 				writeBytes := make([]byte, len(strings.TrimRight(body, "\x00")))
 				copy(writeBytes, body)
 				err := os.WriteFile(fileName, writeBytes, 0644)
 				if err != nil {
-					conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+					writeResponse(conn, "HTTP/1.1 500 Internal Server Error", resHeader, "", closeCon)
 					break
 				}
+
+				writeResponse(conn, "HTTP/1.1 201 Created", map[string]string{}, "", closeCon)
 				conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
 			}
 		default:
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			writeResponse(conn, "HTTP/1.1 404 Not Found", resHeader, "", closeCon)
 		}
 
 		if closeCon {
+			conn.Close()
 			return
 		}
+
+		clear(resHeader)
+	}
+}
+
+func writeResponse(conn net.Conn, reqLine string, headers map[string]string, body string, isClose bool) {
+	headerStr := ""
+	reqLine = reqLine + "\r\n"
+
+	for k, v := range headers {
+		headerStr = headerStr + k + ": " + v + "\r\n"
 	}
 
+	if isClose {
+		headerStr = headerStr + "Connection: Close\r\n"
+	}
+
+	// End of headers
+	headerStr = headerStr + "\r\n"
+
+	conn.Write([]byte(reqLine + headerStr + body))
 }
